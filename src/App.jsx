e96e44946bb36b747a589b690472dc51ee9696a0
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-08-20 21:54:10 +0900

    Container.jsxを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./css/reset.css";
 import "./css/common.css";
+import "./css/styles.css";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div>
       <Header />
+      <Container />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/styles.css";
 
 export default function Header() {
   return <header>College Sensor</header>;
