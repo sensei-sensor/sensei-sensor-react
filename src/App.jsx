c04799dfe032bec922275c61bc61daf637f0b796
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-10-20 22:53:46 +0900

    localStorageの追加の部分をAppに移動とlocalStorageの長さが0のときにボタンを表示させるように

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import UserPage from "./pages/UserPage";
 
 export default function App() {
+  useEffect(() => {
+    localStorage.getItem("groupId")
+      ? JSON.parse(localStorage.getItem("groupId"))
+      : localStorage.setItem("groupId", JSON.stringify([]));
+  }, []);
+
   return (
     <BrowserRouter>
       <Switch>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,40 @@
+import { Box, Button, Container } from "@mui/material";
 import React from "react";
 import GenericTemplate from "../GenericTemplate.jsx";
+import AddGroupIdModal from "../components/main/AddGroupIdModal";
 import GroupContainer from "../components/main/GroupContainer";
 
 export default function MainPage() {
-  return (
-    <GenericTemplate>
-      <GroupContainer />
-      <GroupContainer />
-    </GenericTemplate>
-  );
+  const [groupOpen, setGroupOpen] = React.useState(false);
+  const handleGroupOpen = () => setGroupOpen(true);
+  const handleGroupClose = () => setGroupOpen(false);
+  const groupList = JSON.parse(localStorage.getItem("groupId"));
+
+  if (groupList.length === 0) {
+    return (
+      <GenericTemplate>
+        <Container>
+          <Box
+            borderRadius={5}
+            m={3}
+            p={2}
+            textAlign={"center"}
+            bgcolor={"white"}
+          >
+            <Button color="primary" onClick={handleGroupOpen}>
+              <Box fontWeight={700}>グループIDの追加</Box>
+            </Button>
+          </Box>
+        </Container>
+        <AddGroupIdModal open={groupOpen} handleClose={handleGroupClose} />
+      </GenericTemplate>
+    );
+  } else {
+    return (
+      <GenericTemplate>
+        <GroupContainer />
+        <GroupContainer />
+      </GenericTemplate>
+    );
+  }
 }
