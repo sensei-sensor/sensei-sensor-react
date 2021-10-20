995d222c23a4e861a8a0573f94a175bb43c0be33
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-10-20 13:01:34 +0900

    グループID追加用のモーダルを追加した

--- src/components/common/HeaderBer.jsx
+++ src/components/common/HeaderBer.jsx
@@ -8,6 +8,7 @@ import {
 } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import AddGroupIdModal from "../main/AddGroupIdModal";
 import LoginModal from "../main/LoginModal";
 
 const title = {
@@ -17,9 +18,12 @@ const title = {
 };
 
 export default function HeaderBer() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [loginOpen, setLoginOpen] = React.useState(false);
+  const handleLoginOpen = () => setLoginOpen(true);
+  const handleLoginClose = () => setLoginOpen(false);
+  const [groupOpen, setGroupOpen] = React.useState(false);
+  const handleGroupOpen = () => setGroupOpen(true);
+  const handleGroupClose = () => setGroupOpen(false);
 
   const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
@@ -36,10 +40,14 @@ export default function HeaderBer() {
           >
             <Box fontWeight={700}>College Sensor</Box>
           </Typography>
-          <Button color="primary" onClick={handleOpen}>
+          <Button color="primary" onClick={handleGroupOpen}>
+            <Box fontWeight={700}>グループIDの追加</Box>
+          </Button>
+          <Button color="primary" onClick={handleLoginOpen}>
             <Box fontWeight={700}>先生向けログイン</Box>
           </Button>
-          <LoginModal open={open} handleClose={handleClose} />
+          <LoginModal open={loginOpen} handleClose={handleLoginClose} />
+          <AddGroupIdModal open={groupOpen} handleClose={handleGroupClose} />
         </Toolbar>
       </AppBar>
       <Offset />
---
+++ src/components/main/AddGroupIdModal.jsx
@@ -0,0 +1,97 @@
+import {
+  Backdrop,
+  Box,
+  Button,
+  Fade,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function AddGroupIdModal(props) {
+  useEffect(() => {
+    console.log(localStorage.getItem("groupId"));
+    localStorage.getItem("groupId")
+      ? JSON.parse(localStorage.getItem("groupId"))
+      : localStorage.setItem("groupId", JSON.stringify([]));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const groupIdList = JSON.parse(localStorage.getItem("groupId"));
+    console.log(groupIdList);
+
+    // eslint-disable-next-line no-console
+    console.log(data.get("groupId"));
+
+    groupIdList.push = String(data.get("groupId"));
+    localStorage.setItem("groupId", JSON.stringify(groupIdList));
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="add-group-id-modal"
+        aria-describedby="add-a-group-id"
+        open={props.open}
+        onClose={props.handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 100,
+        }}
+      >
+        <Fade in={props.open}>
+          <Box sx={style}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                グループの追加
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  id="groupId"
+                  label="グループID"
+                  name="groupId"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  グループを追加
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
