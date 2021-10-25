8c339677642f018a09065ad22f67ce0cfb594aad
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-10-25 15:22:49 +0900

    ヘッダーの色とリンクの部分を修正

--- src/components/common/HeaderBer.jsx
+++ src/components/common/HeaderBer.jsx
@@ -6,40 +6,41 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AddGroupIdModal from "../main/AddGroupIdModal";
 import LoginModal from "../main/LoginModal";
 
-const title = {
-  flexGrow: 1,
-  fontWeight: 700,
-  textDecoration: "none",
-};
-
 export default function HeaderBer(props) {
-  const [loginOpen, setLoginOpen] = React.useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
   const handleLoginOpen = () => setLoginOpen(true);
   const handleLoginClose = () => setLoginOpen(false);
 
-  const [groupOpen, setGroupOpen] = React.useState(false);
+  const [groupOpen, setGroupOpen] = useState(false);
   const handleGroupOpen = () => setGroupOpen(true);
   const handleGroupClose = () => setGroupOpen(false);
 
   const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
+  const title = {
+    fontWeight: 700,
+    textDecoration: "none",
+    color: "primary.main",
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar color={"default"}>
+      <AppBar color={"inherit"}>
         <Toolbar>
           <Typography
             variant="h5"
-            color={"primary"}
-            sx={title}
-            component={Link}
-            to={"/"}
+            sx={{
+              flexGrow: 1,
+            }}
           >
-            <Box fontWeight={700}>College Sensor</Box>
+            <Box sx={title} component={Link} to={"/"}>
+              College Sensor
+            </Box>
           </Typography>
           <Button color="primary" onClick={handleGroupOpen}>
             <Box fontWeight={700}>グループIDの追加</Box>
