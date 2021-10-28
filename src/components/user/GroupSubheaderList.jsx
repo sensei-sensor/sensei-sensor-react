74b0e2eee38160dffb7e5477586ae67a75f5bee2
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-10-28 15:43:07 +0900

    GroupNestedListを追加

--- src/components/user/GroupSubheaderList.jsx
+++ src/components/user/GroupSubheaderList.jsx
@@ -1,5 +1,5 @@
 import { List, ListItem, ListItemText, ListSubheader } from "@mui/material";
-import * as React from "react";
+import React from "react";
 
 export default function GroupSubheaderList() {
   return (
