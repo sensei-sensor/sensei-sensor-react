3ebd12b49f521429e9ea208f925c93b386cb4fcc
Naoki Takahashi <aho.mickey.mein@gmail.com>
2022-01-25 04:13:35 +0000

    day.jsの追加

--- src/components/main/TeacherCard.jsx
+++ src/components/main/TeacherCard.jsx
@@ -2,6 +2,8 @@ import { Box, Card, CardContent, Typography } from "@mui/material";
 import React from "react";
 
 export default function TeacherCard(props) {
+  const convertRelativeTime = () => {};
+
   return (
     <Box m={2}>
       <Card sx={{ minWidth: 250, width: "fit-content" }} variant="outlined">
