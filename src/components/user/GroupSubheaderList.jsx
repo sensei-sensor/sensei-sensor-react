fe011d4882fb7b8e46f62fec88bc83682fa2c4fd
Naoki Takahashi <aho.mickey.mein@gmail.com>
2022-01-31 05:58:50 +0000

    UserListのAPIを取得するコードを追加

--- src/components/user/GroupSubheaderList.jsx
+++ src/components/user/GroupSubheaderList.jsx
@@ -1,33 +1,52 @@
 import { List, ListItem, ListItemText, ListSubheader } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function GroupSubheaderList() {
-  return (
-    <List
-      sx={{
-        width: "100%",
-        position: "relative",
-        overflow: "auto",
-        maxHeight: 300,
-        background: "background.paper",
-        border: "1px solid #C1C1C1",
-        borderRadius: "4px",
-        "& ul": { padding: 0 },
-      }}
-      subheader={<li />}
-    >
-      {["機械", "電気", "情報", "建設", "化学"].map((sectionId) => (
-        <li key={`section-${sectionId}`}>
-          <ul>
-            <ListSubheader>{`${sectionId}コース`}</ListSubheader>
-            {[0, 1, 2].map((item) => (
-              <ListItem key={`item-${sectionId}-${item}`}>
-                <ListItemText primary={`Item ${item}`} />
-              </ListItem>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </List>
-  );
+  const [userList, setUserList] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        import.meta.env.VITE_API_HOST +
+          "sensei-sensor-php/WebAPI/users/userList/",
+        { withCredentials: true }
+      )
+      .then((response) => [setUserList(response.data)]);
+  }, []);
+
+  if (userList === null) {
+    return null;
+  } else {
+    return (
+      <List
+        sx={{
+          width: "100%",
+          position: "relative",
+          overflow: "auto",
+          maxHeight: 300,
+          background: "background.paper",
+          border: "1px solid #C1C1C1",
+          borderRadius: "4px",
+          "& ul": { padding: 0 },
+        }}
+        subheader={<li />}
+      >
+        {Object.keys(userList).map((key) => (
+          <li key={`section-${userList[key].courseId}`}>
+            <ul>
+              <ListSubheader>{userList[key].courseName}</ListSubheader>
+              {Object.keys(userList[key].users).map((item) => (
+                <ListItem key={`item-${key}-${item}`}>
+                  <ListItemText
+                    primary={`${userList[key].users[item].userName}`}
+                  />
+                </ListItem>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </List>
+    );
+  }
 }
