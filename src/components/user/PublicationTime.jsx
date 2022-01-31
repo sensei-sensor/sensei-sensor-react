916dacf12a56b629f7ba40dbdb320687ed14530d
Naoki Takahashi <aho.mickey.mein@gmail.com>
2022-01-31 21:14:05 +0900

    タイポと初期値の修正

--- src/components/user/PublicationTime.jsx
+++ src/components/user/PublicationTime.jsx
@@ -10,10 +10,7 @@ import axios from "axios";
 import React, { useEffect } from "react";
 
 export default function PublicationTime() {
-  const [publicationTime, setPublicationTime] = React.useState({
-    start: 9,
-    end: 17,
-  });
+  const [publicationTime, setPublicationTime] = React.useState(null);
 
   const handleStartTimeChange = (event) => {
     axios
@@ -60,47 +57,50 @@ export default function PublicationTime() {
           "sensei-sensor-php/WebAPI/users/publicationTime/",
         { withCredentials: true }
       )
-      .then((responce) => {
-        console.log;
+      .then((response) => {
         setPublicationTime({
-          start: responce.data.publicationTime.start / 60,
-          end: responce.data.publicationTime.end / 60,
+          start: response.data.publicationTime.start / 60,
+          end: response.data.publicationTime.end / 60,
         });
       });
   }, []);
 
-  return (
-    <Box display={"flex"} alignItems={"center"}>
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
-        <InputLabel>開始時間</InputLabel>
-        <Select
-          value={publicationTime.start}
-          onChange={handleStartTimeChange}
-          autoWidth
-          label="開始時間"
-        >
-          <MenuItem value={8}>8時</MenuItem>
-          <MenuItem value={9}>9時</MenuItem>
-          <MenuItem value={10}>10時</MenuItem>
-          <MenuItem value={11}>11時</MenuItem>
-        </Select>
-      </FormControl>
-      <Typography>から</Typography>
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
-        <InputLabel>終了時間</InputLabel>
-        <Select
-          value={publicationTime.end}
-          onChange={handleEndTimeChange}
-          autoWidth
-          label="終了時間"
-        >
-          <MenuItem value={15}>15時</MenuItem>
-          <MenuItem value={16}>16時</MenuItem>
-          <MenuItem value={17}>17時</MenuItem>
-          <MenuItem value={18}>18時</MenuItem>
-        </Select>
-      </FormControl>
-      <Typography>まで</Typography>
-    </Box>
-  );
+  if (publicationTime === null) {
+    return null;
+  } else {
+    return (
+      <Box display={"flex"} alignItems={"center"}>
+        <FormControl sx={{ m: 1, minWidth: 80 }}>
+          <InputLabel>開始時間</InputLabel>
+          <Select
+            value={publicationTime.start}
+            onChange={handleStartTimeChange}
+            autoWidth
+            label="開始時間"
+          >
+            <MenuItem value={8}>8時</MenuItem>
+            <MenuItem value={9}>9時</MenuItem>
+            <MenuItem value={10}>10時</MenuItem>
+            <MenuItem value={11}>11時</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography>から</Typography>
+        <FormControl sx={{ m: 1, minWidth: 80 }}>
+          <InputLabel>終了時間</InputLabel>
+          <Select
+            value={publicationTime.end}
+            onChange={handleEndTimeChange}
+            autoWidth
+            label="終了時間"
+          >
+            <MenuItem value={15}>15時</MenuItem>
+            <MenuItem value={16}>16時</MenuItem>
+            <MenuItem value={17}>17時</MenuItem>
+            <MenuItem value={18}>18時</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography>まで</Typography>
+      </Box>
+    );
+  }
 }
