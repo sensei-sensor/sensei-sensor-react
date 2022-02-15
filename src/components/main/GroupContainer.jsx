dc8dff537a675fac2cf38b07e77ed4e379d3d6ab
Naoki Takahashi <aho.mickey.mein@gmail.com>
2022-02-15 20:38:33 +0900

    未検出時のタイトルを赤に

--- src/components/main/GroupContainer.jsx
+++ src/components/main/GroupContainer.jsx
@@ -7,7 +7,13 @@ export default function GroupContainer(props) {
     <Container>
       <Box m={3} pt={2} borderRadius={5} textAlign={"center"} bgcolor={"white"}>
         <Typography variant={"h5"}>
-          <Box fontWeight={700}>{props.groupName}</Box>
+          {props.groupName === "未検出" ? (
+            <Box fontWeight={700} color={"red"}>
+              {props.groupName}
+            </Box>
+          ) : (
+            <Box fontWeight={700}>{props.groupName}</Box>
+          )}
         </Typography>
         <Box display={"flex"} flexWrap={"wrap"} justifyContent={"center"}>
           {Object.keys(props.users).map((key) => {
